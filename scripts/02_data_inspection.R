a604fc15af71bd565b6f404e0429c4ab6e19a399
Dantz Farrow <dantzfarrow@gmail.com>
2026-03-28 14:33:18 -0500

    Add to do list for phase 4 of EDA; Overwrite cleaned csv to remove row names

--- scripts/02_data_inspection.R
+++ scripts/02_data_inspection.R
@@ -1,7 +1,7 @@
 #####################################
-### File: 02_data_inspection.R
+### File:   02_data_inspection.R
 ### Author: Dantz Farrow
-### Date: 2026-03-27 
+### Date:   2026-03-27 
 #####################################
 
 ### Load
@@ -11,4 +11,4 @@ df <- read.csv("data/snap_enrollment_oklahoma.csv")
 dim(df)      # Shape: Rows and columns
 str(df)      # Structure: Data types and column names
 head(df)     # Preview: First few rows
-summary(df)  # Summary statistics: Min, Max, Mean, etc.
+summary(df)  # Summary statistics: Min, Max, Mean, etc.; No missing values
--- scripts/03_data_cleaning.R
+++ scripts/03_data_cleaning.R
@@ -1,22 +1,26 @@
 #####################################
-### File:   03_data_cleaning.R
-### Author: Dantz Farrow
-### Date:   2026-03-28 
+### File:        03_data_cleaning.R
+### Author:      Dantz Farrow
+### Description: Clean and prepare SNAP enrollment data for analysis, 
+###              including data type conversions and preliminary investigation.
+### Date:        2026-03-28 
 #####################################
 
 ### Load
 df <- read.csv("data/snap_enrollment_oklahoma.csv")
 
 ### Review
+paste0(min(df_clean$Date), " - ", max(df_clean$Date)) # Jan12 - Jan19
 colnames(df)
 class(df$Date)
 class(df$FIPS)
 
 ### Clean data types
-df$Date <- as.Date(df$Date, format = "%Y-%m-%d") # Convert Date to Date
+df_clean <- df # copy original for cleaning
+df_clean$Date <- as.Date(df_clean$Date, format = "%Y-%m-%d") # Convert Date to Date
 class(df_clean$Date) # Verify Date type
 
-df$FIPS <- as.character(df$FIPS) # Convert FIPS to char
+df_clean$FIPS <- as.character(df_clean$FIPS) # Convert FIPS to char
 class(df_clean$FIPS) # Verify char type 
 
 ### Investigate SubCounty == 2
@@ -27,3 +31,13 @@ nrow(df_clean[df_clean$FIPS == '40109' & df_clean$SubCounty != 2, ]) # all FIPS 
 chk_all_issuance <- df_clean[df_clean$SNAP_All_Issuance == 0, ]
 unique(chk_all_issuance$FIPS) # 13 distinct FIPS with zero issuance
 paste0("Date Range: ", min(chk_all_issuance$Date), " to ", max(chk_all_issuance$Date)) # show date range
+
+### TODO: 
+##  - 15 time periods across 7 years (plus Jan2019); 1155 observations doesn't add up.
+##  - NPA participation vs PA participation
+##  - Participation vs issuance; are there instances in which particpation > issuance? Why?
+##  - For BigQuery: Is Oklahoma anomolous vis-a-vis Program == 'EBT'?
+##  - How does SNAP participation change over time across Oklahoma?
+
+### Export cleaned dataset
+write.csv(df_clean, "data/snap_enrollment_oklahoma_clean.csv", row.names = FALSE)
